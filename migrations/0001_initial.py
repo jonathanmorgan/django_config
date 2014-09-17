f2960f34c01beac476f11e5b7be15f5a75ddd559
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2014-09-17 13:25:03 -0400

    Updates for django 1.7.

--- migrations/0001_initial.py
+++ migrations/0001_initial.py
@@ -1,40 +1,27 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Config_Property'
-        db.create_table(u'django_config_config_property', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('application', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('property_name', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('property_value', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('create_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-            ('last_update', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
-        ))
-        db.send_create_signal(u'django_config', ['Config_Property'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Config_Property'
-        db.delete_table(u'django_config_config_property')
-
-
-    models = {
-        u'django_config.config_property': {
-            'Meta': {'object_name': 'Config_Property'},
-            'application': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'create_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_update': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
-            'property_name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'property_value': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
-        }
-    }
-
-    complete_apps = ['django_config']
+    operations = [
+        migrations.CreateModel(
+            name='Config_Property',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('application', models.CharField(max_length=255)),
+                ('property_name', models.CharField(max_length=255)),
+                ('property_value', models.TextField(null=True, blank=True)),
+                ('create_date', models.DateTimeField(auto_now_add=True)),
+                ('last_update', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
