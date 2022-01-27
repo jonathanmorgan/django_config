49f5b7c98228deaeb0ed37b2fd4ceeaf49d47d6f
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2022-01-27 12:49:52 -0500

    more updates to support property group.

--- models/abstract_config_property.py
+++ models/abstract_config_property.py
@@ -29,10 +29,10 @@ class Abstract_Config_Property( models.Model ):
     - property_name
     - property_value
     - property_type
-    
+
     It also provides methods for interacting with configuration properties, such
        that you can set a default as part of the call, and so deal with the
-       property not yet having been set. 
+       property not yet having been set.
     '''
 
     #---------------------------------------------------------------------------
@@ -41,7 +41,7 @@ class Abstract_Config_Property( models.Model ):
 
     APPLICATION_CORE = "core" # Application you can use for site-wide settings.
     APPLICATION_DEFAULT = APPLICATION_CORE
-    
+
     # property types
     TYPE_NONE = None
     TYPE_STRING = "string"
@@ -49,7 +49,7 @@ class Abstract_Config_Property( models.Model ):
     TYPE_DECIMAL = "decimal"
     TYPE_BOOLEAN = "boolean"
     TYPE_DEFAULT = TYPE_NONE
-    
+
     TYPE_CHOICES = (
         ( TYPE_NONE, 'None' ),
         ( TYPE_STRING, 'String' ),
@@ -90,279 +90,372 @@ class Abstract_Config_Property( models.Model ):
 
 
     @classmethod
-    def get_property_boolean_value( cls, application_IN, property_name_IN, default_IN = False, *args, **kwargs ):
-        
+    def get_property_boolean_value( cls,
+                                    application_IN,
+                                    property_name_IN,
+                                    default_IN = False,
+                                    property_group_IN = None,
+                                    *args,
+                                    **kwargs ):
+
         # return reference
         value_OUT = None
-        
+
         # declare variables
         prop_value = None
-        
+
         # get property
-        prop_value = cls.get_property_value( application_IN, property_name_IN, default_IN )
+        prop_value = cls.get_property_value( application_IN,
+                                             property_name_IN,
+                                             default_IN = default_IN,
+                                             property_group_IN = property_group_IN )
 
         # convert to boolean if not None.
         if ( ( prop_value ) and ( prop_value != None ) ):
-            
+
             # Not none - see if it is a known value for True.
             if ( ( prop_value == True )
                 or ( ( isinstance( prop_value, six.integer_types ) == True ) and ( prop_value == 1 ) )
                 or ( ( isinstance( prop_value, six.string_types ) == True ) and ( prop_value.lower() == "true" ) ) ):
-            
+
                 # true.
                 value_OUT = True
-                
+
             else:
-            
+
                 # false.  Or unknown. Which is false.
                 value_OUT = False
 
             #-- END check to see if true or false. --#
-            
+
         else:
-            
+
             # None - convert default to int.
             value_OUT = default_IN
-            
-        #-- END check to make sure we don't try to convert None to int. --#       
-        
+
+        #-- END check to make sure we don't try to convert None to int. --#
+
         return value_OUT
-        
+
     #-- END method get_property_boolean_value() --#
 
 
     @classmethod
-    def get_property_int_value( cls, application_IN, property_name_IN, default_IN = 0, *args, **kwargs ):
-        
+    def get_property_int_value( cls,
+                                application_IN,
+                                property_name_IN,
+                                default_IN = 0,
+                                property_group_IN = None,
+                                *args,
+                                **kwargs ):
+
         # return reference
         value_OUT = None
-        
+
         # declare variables
         prop_value = None
-        
+
         # get property
-        prop_value = cls.get_property_value( application_IN, property_name_IN, default_IN )
+        prop_value = cls.get_property_value( application_IN,
+                                             property_name_IN,
+                                             default_IN = default_IN,
+                                             property_group_IN = property_group_IN )
 
         # convert to int if not None.
         if ( ( prop_value ) and ( prop_value != None ) ):
-            
+
             # Not none - convert to int.
             value_OUT = int( prop_value )
-            
+
         else:
-            
+
             # None - convert default to int.
             value_OUT = int( default_IN )
-            
-        #-- END check to make sure we don't try to convert None to int. --#       
-        
+
+        #-- END check to make sure we don't try to convert None to int. --#
+
         return value_OUT
-        
+
     #-- END method get_property_int_value() --#
 
 
     @classmethod
-    def get_property_list_value( cls, application_IN, property_name_IN, default_IN = None, delimiter_IN = ",", *args, **kwargs ):
-        
+    def get_property_list_value( cls,
+                                 application_IN,
+                                 property_name_IN,
+                                 default_IN = None,
+                                 delimiter_IN = ",",
+                                 property_group_IN = None,
+                                 *args,
+                                 **kwargs ):
+
         # return reference
         value_OUT = None
-        
+
         # declare variables
         prop_value = None
-        
+
         # get property
-        prop_value = cls.get_property_value( application_IN, property_name_IN, default_IN )
+        prop_value = cls.get_property_value( application_IN,
+                                             property_name_IN,
+                                             default_IN = default_IN,
+                                             property_group_IN = property_group_IN )
 
         # convert to int if not None.
         if ( prop_value != None ):
-            
+
             # Not none - convert to int.
             value_OUT = ListHelper.get_value_as_list( prop_value, delimiter_IN )
-            
+
         else:
-            
+
             # None - empty list.
             value_OUT = []
-            
-        #-- END check to make sure we don't try to convert None to list. --#       
-        
+
+        #-- END check to make sure we don't try to convert None to list. --#
+
         return value_OUT
-        
+
     #-- END method get_property_list_value() --#
 
 
     @classmethod
-    def get_property_value( cls, application_IN, property_name_IN, default_IN = None, *args, **kwargs ):
-        
+    def get_property_value( cls,
+                            application_IN,
+                            property_name_IN,
+                            default_IN = None,
+                            property_group_IN = None,
+                            *args,
+                            **kwargs ):
+
         # return reference
         value_OUT = None
-        
+
         # declare variables
         prop_qs = None
         prop_value = None
-        
+
         # make sure we have an application
         if ( ( application_IN ) and ( application_IN != "" ) ):
 
             # make sure we have a property name
             if ( ( property_name_IN ) and ( property_name_IN != "" ) ):
-                
+
                 # got one.  look for associated attributes with that name.
-                
+
                 # get QuerySet of attributes for this Object.
                 prop_qs = cls.objects.all()
-                
+
                 # filter based on application and property name passed in.
                 prop_qs = prop_qs.filter( application = application_IN )
                 prop_qs = prop_qs.filter( property_name = property_name_IN )
-                
+
+                # got a group? if so, filter on it, too.
+                if ( property_group_IN is not None ):
+
+                    # filter on group
+                    prop_qs = prop_qs.filter( property_group = property_group_IN )
+
+                #-- END check if there is a property group specified. --#
+
                 # anything in it?
                 if ( ( prop_qs is not None ) and ( prop_qs.count() > 0 ) ):
-                    
+
                     # got at least one attribute.  For now, grab first one.
                     prop_value = prop_qs[ 0 ]
-                    
+
                     # return value in that instance
                     value_OUT = prop_value.property_value
-                    
+
                 else:
-                    
+
                     # no attribute for that name.  If default, return it.
                     if ( default_IN != None ):
-                        
+
                         value_OUT = default_IN
-                        
+
                     #-- END check to see if default --#
-                    
+
                 #-- END check to see if any matching attributes. --#
-            
+
             else:
-                
+
                 # no name - return None.
                 value_OUT = None
-                
+
             #-- END check to see if name. --#
 
         else:
-            
+
             # no application - return None.
             value_OUT = None
-            
+
         #-- END check to see if application. --#
-        
+
         return value_OUT
-        
+
     #-- END method get_property_value() --#
 
 
     @classmethod
-    def set_property_value( cls, application_IN, property_name_IN, value_IN, *args, **kwargs ):
-        
+    def set_property_value( cls,
+                            application_IN,
+                            property_name_IN,
+                            value_IN,
+                            property_group_IN = None,
+                            *args,
+                            **kwargs ):
+
         # return reference
         success_OUT = False
-        
+
         # declare variables
         prop_qs = None
         prop_value = None
-        
+
         # make sure we have an application
         if ( ( application_IN ) and ( application_IN != "" ) ):
 
             # make sure we have a property name
             if ( ( property_name_IN ) and ( property_name_IN != "" ) ):
-                
+
                 # got one.  look for associated attributes with that name.
-                
+
                 # get QuerySet of properties.
                 prop_qs = cls.objects.all()
-                
+
                 # filter based on application and name passed in.
                 prop_qs = prop_qs.filter( application = application_IN )
                 prop_qs = prop_qs.filter( property_name = property_name_IN )
-                
+
+                # got a group? if so, filter on it, too.
+                if ( property_group_IN is not None ):
+
+                    # filter on group
+                    prop_qs = prop_qs.filter( property_group = property_group_IN )
+
+                #-- END check if there is a property group specified. --#
+
                 # anything in it?
                 if ( ( prop_qs is not None ) and ( prop_qs.count() > 0 ) ):
-                    
+
                     # got at least one property.  For now, grab first one.
                     prop_value = prop_qs[ 0 ]
-                    
+
                 else:
-                    
+
                     # no property for that application/name.  Make a new one.
                     prop_value = cls()
-                    
+
                     # set values
                     prop_value.application = application_IN
                     prop_value.property_name = property_name_IN
-                    
+                    prop_value.property_group = property_group_IN
+
                 #-- END check to see if any matching properties. --#
-                
+
                 # property retrieved/made.  Set value and save.
                 prop_value.property_value = value_IN
                 prop_value.save()
                 success_OUT = True
-            
+
             else:
-                
+
                 # no name - not success.
                 success_OUT = False
-                
+
             #-- END check to see if name. --#
-        
+
         else:
-            
+
             # no application - not success.
             success_OUT = False
-            
+
         #-- END check to see if we have an application --#
-        
+
         return success_OUT
-        
+
     #-- END method set_property_value() --#
 
-    
+
     #----------------------------------------------------------------------
     # instance methods
     #----------------------------------------------------------------------
 
 
     def __str__( self ):
-        
+
         # return reference
-        string_OUT = ''
+        string_OUT = ""
+        current_name = None
+        current_value = None
         prefix = ""
-        
-        if ( self.id is not None ):
-        
-            string_OUT = str( self.id )
+
+        # id
+        current_name = "id"
+        current_value = self.id
+        if ( current_value is not None ):
+
+            string_OUT = str( current_value )
             prefix = " - "
-            
+
         #-- END check for ID --#
-        
-        if ( self.application ):
 
-            string_OUT += prefix + "application: " + self.application
+        # application
+        current_name = "application"
+        current_value = self.application
+        if ( current_value ):
+
+            string_OUT += "{prefix}{my_name}: {my_value}".format(
+                prefix = prefix,
+                my_name = current_name,
+                my_value = current_value )
             prefix = "; "
-            
+
         #-- END check for application --#
-            
-        if ( self.property_name ):
 
-            string_OUT += prefix + "prop name: " + self.property_name
+        # property group
+        current_name = "prop group"
+        current_value = self.property_group
+        if ( current_value ):
+
+            string_OUT += "{prefix}{my_name}: {my_value}".format(
+                prefix = prefix,
+                my_name = current_name,
+                my_value = current_value )
             prefix = "; "
-            
+
+        #-- END check for property group --#
+
+        # property name
+        current_name = "prop name"
+        current_value = self.property_name
+        if ( current_value ):
+
+            string_OUT += "{prefix}{my_name}: {my_value}".format(
+                prefix = prefix,
+                my_name = current_name,
+                my_value = current_value )
+            prefix = "; "
+
         #-- END check for property_name field --#
 
-        if ( self.property_value ):
+        # property value
+        current_name = "prop value"
+        current_value = self.property_value
+        if ( current_value ):
 
-            string_OUT += prefix + "prop value: " + self.property_value
+            string_OUT += "{prefix}{my_name}: {my_value}".format(
+                prefix = prefix,
+                my_name = current_name,
+                my_value = current_value )
             prefix = "; "
-            
+
         #-- END check for property_value field --#
 
         return string_OUT
-        
+
     #-- END __str__() method --#
-        
+
 
 #= END Abstract_Config_Property Model =========================================#
